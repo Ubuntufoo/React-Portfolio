368cf212b97b9ebecf3bc94a6b2525a64fd8c8f5
Timothy Murphy <TimothyMurphy123@gmail.com>
2024-05-03 18:01:20 -0400

    fullpage scaffolding set up

--- React-Portfolio-Project/src/App.jsx
+++ React-Portfolio-Project/src/App.jsx
@@ -1,17 +1,44 @@
 //
 
-import { useState } from 'react'
+import ReactFullpage from "@fullpage/react-fullpage";
+import "./index.css";
 
-import './App.css'
+const anchors = ["intro", "skills", "projects", "roadmap"];
 
-function App() {
+const App = () => (
+  <ReactFullpage
+    licenseKey={"gplv3-license"}
+    scrollingSpeed={10} /* Options here */
+    anchors={anchors}
+    navigation
+    navigationTooltips={anchors}
+    sectionsColor={["#7fff00", "#00ffff", "#29ab87", "#476999"]}
+    onLeave={(origin, destination, direction) => {
+      console.log("onLeave event", { origin, destination, direction });
+    }}
+    render={({ state, fullpageApi }) => {
+      console.log("render prop change", state, fullpageApi);
 
-
-  return (
-    <>
-
-    </>
-  )
-}
-
-export default App
+      return (
+        <div>
+          <div className="section">
+            <div className="slide"> Slide 11111111</div>
+            <div className="slide"> Slide 22222222</div>
+            <div className="slide"> Slide 3333333</div>
+            <div className="slide "> Slide 4 4444444</div>
+          </div>
+          <div className="section">
+            Section 2
+          </div>
+          <div className="section">
+            Section 3
+          </div>
+          <div className="section">
+            Section 4
+          </div>
+        </div>
+      );
+    }}
+  />
+);
+export default App;
