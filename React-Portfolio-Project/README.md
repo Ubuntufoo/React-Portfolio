05a7910ffee8e9b508d2e736280fefa1f3ba4a41
Timothy Murphy <TimothyMurphy123@gmail.com>
2024-05-10 22:46:26 -0400

    chore: Update SideBar component and CSS styles for contact information

--- React-Portfolio-Project/README.md
+++ React-Portfolio-Project/README.md
@@ -2,19 +2,3 @@
 
 npm WARN deprecated @babel/polyfill@7.12.1: 🚨 This package has been deprecated in favor of separate inclusion of a polyfill and regenerator-runtime (when needed). See the @babel/polyfill docs (https://babeljs.io/docs/en/babel-polyfill) for more information.
 npm WARN deprecated core-js@2.6.12: core-js@<3.23.3 is no longer maintained and not recommended for usage due to the number of issues. Because of the V8 engine whims, feature detection in old core-js versions could cause a slowdown up to 100x even if nothing is polyfilled. Some versions have web compatibility issues. Please, upgrade your dependencies to the actual version of core-js.
-
-.arrow-container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 50px;
-  /* Adjust width as needed */
-  height: 50px;
-  /* Adjust height as needed */
-  background-color: #383838;
-  /* Background color of the container */
-  border-radius: 8px;
-  /* Border radius to create rounded corners */
-  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1);
-  /* Box shadow for the raised effect */
-}
--- React-Portfolio-Project/src/components/SideBar.jsx
+++ React-Portfolio-Project/src/components/SideBar.jsx
@@ -1,29 +1,45 @@
 // a contact me component with an avatar
 
 import { FaGithub } from 'react-icons/fa'
+import { SiMinutemailer } from 'react-icons/si'
+import { PiMailboxFill } from 'react-icons/pi'
+
 
 export default function SideBar() {
   return (
-    <div className="absolute top-0 -ms-64 h-0 w-0 cursor-pointer border-b-[440px] border-l-[380px] border-t-[140px] border-solid border-b-transparent border-l-[#fff] border-t-transparent opacity-90 transition-transform duration-1000 hover:translate-x-36">
+    <div className="absolute top-0 -ms-80 h-0 w-0 cursor-pointer border-b-[410px] border-l-[440px] border-t-[200px] border-solid border-b-transparent border-l-[#fff] border-t-transparent opacity-90 transition-transform duration-1000 translate-x-56">
       <p className="absolute right-9 text-xl font-semibold text-gray-800 hover:font-bold hover:text-black">
         Contact
       </p>
       <div
-        className="absolute -ms-96 h-0 w-0 cursor-pointer border-b-[300px] border-l-[260px] border-t-[90px]
-          border-solid border-b-transparent border-l-black border-t-transparent transition-transform duration-1000 hover:translate-x-28"
+        className="absolute -ms-[590px] h-0 w-0 cursor-pointer border-b-[300px] border-l-[320px] border-t-[90px]
+          border-solid border-b-transparent border-l-gray-800 border-t-transparent transition-transform duration-1000 hover:translate-x-44"
       >
-        <p>
-          <FaGithub className="absolute right-10 text-4xl text-white" />
-        </p>
+        <div className="absolute right-8 -mt-1 flex items-center space-x-3 text-white">
+          <a href="https://github.com/Ubuntufoo" className="font-mono leading-relaxed text-nowrap">
+            Github Repository
+          </a>
+          <FaGithub className="text-3xl" />
+        </div>
       </div>
       <div
-        className="absolute -ms-96 mt-32 h-0 w-0 cursor-pointer border-b-[300px] border-l-[260px] border-t-[90px]
-          border-solid border-b-transparent border-l-gray-800 border-t-transparent transition-transform duration-1000 hover:translate-x-28"
-      ></div>
+        className="absolute -ms-[590px] mt-36 h-0 w-0 cursor-pointer border-b-[300px] border-l-[320px] border-t-[90px]
+          border-solid border-b-transparent border-l-gray-800 border-t-transparent transition-transform duration-1000 hover:translate-x-60"
+      >
+        <div className="absolute right-8 -mt-1 flex items-center space-x-3 text-white">
+          <p className="font-mono">TimothyMurphy123@gmail.com</p>
+          <SiMinutemailer className="text-3xl" />
+        </div>
+      </div>
       <div
-        className="absolute -ms-96 mt-64 h-0 w-0 cursor-pointer border-b-[300px] border-l-[260px] border-t-[90px]
-          border-solid border-b-transparent border-l-gray-500 border-t-transparent transition-transform duration-1000 hover:translate-x-28"
-      ></div>
+        className="absolute -ms-[590px] mt-72 h-0 w-0 cursor-pointer border-b-[300px] border-l-[320px] border-t-[90px]
+          border-solid border-b-transparent border-l-gray-800 border-t-transparent transition-transform duration-1000 hover:translate-x-44"
+      >
+        <div className="absolute right-8 -mt-1 flex items-center space-x-3 text-white">
+          <p className="text-nowrap font-mono">Hyannis, MA, 02601</p>
+          <PiMailboxFill className="text-3xl" />
+        </div>
+      </div>
     </div>
   )
 }
