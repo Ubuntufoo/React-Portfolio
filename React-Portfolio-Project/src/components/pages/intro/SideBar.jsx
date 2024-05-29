d4d91f4d8c3c39bc7b43c3b62ab136c4b6cedce3
Timothy Murphy <TimothyMurphy123@gmail.com>
2024-05-29 12:36:21 -0400

    sidebar tabs resized

--- React-Portfolio-Project/src/components/pages/intro/SideBar.jsx
+++ React-Portfolio-Project/src/components/pages/intro/SideBar.jsx
@@ -10,9 +10,9 @@ import { GrDocumentUser } from 'react-icons/gr'
 
 export default function SideBar() {
   return (
-    <div className="group absolute bottom-0 left-0 -ms-72 flex size-0 translate-x-28 cursor-pointer flex-col items-end justify-end border-b-[1px] border-l-[390px] border-t-[840px] border-solid border-b-transparent border-l-[#fff] border-t-transparent font-mono transition-transform duration-500">
-      <div className="-mb-48 me-32 h-0 w-0 cursor-pointer border-b-[290px] border-l-[325px] border-t-[1px] border-solid border-b-transparent border-l-gray-800 border-t-transparent transition-transform duration-500 hover:translate-x-44">
-        <div className="absolute -ms-[235px] mt-1.5 flex items-center space-x-3.5 text-white">
+    <div className="group absolute bottom-0 left-0 -ms-72 flex size-0 hover:translate-x-28 cursor-pointer flex-col items-end justify-end border-b-[1px] border-l-[391px] border-t-[600px] border-solid border-b-transparent border-l-[#fff] border-t-transparent font-mono transition-transform duration-500">
+      <div className="-mb-48 me-28 h-0 w-0 cursor-pointer border-b-[290px] border-l-[325px] border-t-[1px] border-solid border-b-transparent border-l-gray-800 border-t-transparent transition-transform duration-500 hover:translate-x-44">
+        <div className="absolute -ms-[251px] mt-2 flex items-center space-x-4 text-white">
           <a
             href="https://github.com/Ubuntufoo"
             target="_blank"
@@ -24,14 +24,19 @@ export default function SideBar() {
           <FaGithub className="text-3xl" />
         </div>
       </div>
-      <div className="-mb-48 me-32 h-0 w-0 cursor-pointer border-b-[290px] border-l-[330px] border-t-[1px] border-solid border-b-transparent border-l-gray-800 border-t-transparent transition-transform duration-500 hover:translate-x-60">
-        <div className="absolute -ms-[316px] mt-1 flex items-center space-x-3.5 text-white">
-          <p>TimothyMurphy123@gmail.com</p>
+      <div className="-mb-48 me-28 h-0 w-0 cursor-pointer border-b-[290px] border-l-[350px] border-t-[1px] border-solid border-b-transparent border-l-gray-800 border-t-transparent transition-transform duration-500 hover:translate-x-60">
+        <div className="absolute -ms-[331px] mt-2 flex items-center space-x-5 text-white">
+          <a
+            href="mailto:TimothyMurphy123@gmail.com"
+            className="text-nowrap leading-relaxed"
+          >
+            TimothyMurphy123@gmail.com
+          </a>
           <SiMinutemailer className="text-3xl" />
         </div>
       </div>
-      <div className="-mb-48 me-32 h-0 w-0 cursor-pointer border-b-[290px] border-l-[325px] border-t-[1px] border-solid border-b-transparent border-l-gray-800 border-t-transparent transition-transform duration-500 hover:translate-x-40">
-        <div className="absolute -ms-[228px] mt-1 flex items-center space-x-4 text-white">
+      <div className="-mb-48 me-28 h-0 w-0 cursor-pointer border-b-[290px] border-l-[325px] border-t-[1px] border-solid border-b-transparent border-l-gray-800 border-t-transparent transition-transform duration-500 hover:translate-x-40">
+        <div className="absolute -ms-[242px] mt-2 flex items-center space-x-5 text-white">
           <a
             href="https://www.linkedin.com/in/timothy-murphy-19a702213/"
             target="_blank"
@@ -43,8 +48,8 @@ export default function SideBar() {
           <BiLogoLinkedinSquare className="text-3xl" />
         </div>
       </div>
-      <div className="-mb-48 me-32 h-0 w-0 cursor-pointer border-b-[290px] border-l-[325px] border-t-[1px] border-solid border-b-transparent border-l-gray-800 border-t-transparent transition-transform duration-500 hover:translate-x-40">
-        <div className="absolute -ms-[185px] mt-1.5 flex items-center space-x-5 text-white">
+      <div className="-mb-64 me-28 h-0 w-0 cursor-pointer border-b-[290px] border-l-[325px] border-t-[1px] border-solid border-b-transparent border-l-gray-800 border-t-transparent transition-transform duration-500 hover:translate-x-40">
+        <div className="absolute -ms-[198px] mt-2 flex items-center space-x-6 text-white">
           <a
             href="https://docs.google.com/document/d/15K8jNlhiDEw9anGdRNiWMGWhdIbsLZinUbh73TV_LD8/edit?usp=sharing"
             target="_blank"
