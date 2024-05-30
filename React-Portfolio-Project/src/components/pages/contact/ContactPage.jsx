7016a0965fe19b56999db79f4c4a04b133a2ebbb
Timothy Murphy <TimothyMurphy123@gmail.com>
2024-05-30 16:47:58 -0400

    added responsive styling (2xl: needs adjusting)

--- React-Portfolio-Project/src/components/pages/contact/ContactPage.jsx
+++ React-Portfolio-Project/src/components/pages/contact/ContactPage.jsx
@@ -13,28 +13,31 @@ export default function ContactPage() {
   return (
     <div className="section h-screen">
       <section className="h-screen w-screen">
-        <div className="absolute inset-0 2xl:py-24">
+        <div className="absolute inset-x-0 bottom-0 h-1/2 2xl:inset-0 2xl:h-full 2xl:py-24">
           <iframe
             className="size-full brightness-95"
             title="Map of My Area"
-            src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d251254.8376529536!2d-70.43794865653597!3d41.77996473715266!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1717037312689!5m2!1sen!2sus"
+            src="https://www.google.com/maps/embed?pb=!1m10!1m8!1m3!1d218593.30925969745!2d-70.32901608707758!3d41.820044176414164!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sus!4v1717097894901!5m2!1sen!2sus"
             allowFullScreen={false}
             referrerPolicy="no-referrer-when-downgrade"
           ></iframe>
         </div>
-        <div className="absolute bottom-12 max-w-[470px] left-36 z-10 flex flex-col p-7 text-white shadow-2xl">
+        <div className="absolute z-10 h-1/2 w-full px-4 text-white shadow-xl 2xl:max-w-[470px]">
           <div className="absolute inset-0 -z-10 bg-cyan-950 opacity-90"></div>
-          <h1 className="text-center text-4xl">Contact Me</h1>
-          <form action="" className="mt-10 text-lg">
-            <div className="grid gap-8 sm:grid-cols-2">
-              <div className="relative z-0">
+
+          <form action="" className="size-full">
+            <div className="mt-6 grid size-full grid-cols-2 gap-x-4 text-sm md:text-base 2xl:mt-10 2xl:gap-8 2xl:text-lg">
+              <h1 className="col-span-full pb-2 text-2xl md:text-4xl 2xl:pt-0 2xl:text-center">
+                Contact Me
+              </h1>
+              <div className="relative">
                 <input
                   type="text"
                   name="name"
                   className="border-1 peer block w-full border-b bg-inherit py-3 font-serif focus:border-gray-950 focus:outline-none focus:ring-0"
                   placeholder=" "
                 />
-                <label className="absolute top-3 -z-10 origin-[0] -translate-y-8 scale-90 transform duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-8 peer-focus:scale-90 peer-focus:text-gray-100 peer-focus:dark:text-gray-100">
+                <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-90 transform duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-90 peer-focus:text-gray-100 peer-focus:dark:text-gray-100">
                   Your Name
                 </label>
               </div>
@@ -45,7 +48,7 @@ export default function ContactPage() {
                   className="border-1 peer block w-full border-b bg-transparent py-3 font-serif focus:border-gray-950 focus:outline-none focus:ring-0"
                   placeholder=" "
                 />
-                <label className="absolute top-3 -z-10 origin-[0] -translate-y-8 scale-90 transform duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-8 peer-focus:scale-90 peer-focus:text-gray-100 peer-focus:dark:text-gray-100">
+                <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-90 transform duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-90 peer-focus:text-gray-100 peer-focus:dark:text-gray-100">
                   Your Email
                 </label>
               </div>
@@ -53,20 +56,19 @@ export default function ContactPage() {
                 <textarea
                   name="message"
                   rows="5"
-                  className="border-1 peer block w-full border-b bg-transparent py-3 font-serif  focus:border-gray-950 focus:outline-none focus:ring-0"
+                  className="border-1 peer block w-full border-b bg-transparent py-3 font-serif focus:border-gray-950 focus:outline-none focus:ring-0"
                   placeholder=" "
                 ></textarea>
-                <label className="absolute top-3 -z-10 origin-[0] -translate-y-8 scale-90 transform duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-8 peer-focus:scale-90 peer-focus:text-gray-100 peer-focus:dark:text-gray-100">
+                <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-90 transform duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-90 peer-focus:text-gray-100 peer-focus:dark:text-gray-100">
                   Your Message
                 </label>
               </div>
+              <div className="col-span-full w-fit ms-auto h-fit bg-cyan-600 px-6 py-2.5 ring-1 ring-gray-950 flex">
+                <button type="submit" className="text-xl tracking-wide">
+                  Send
+                </button>
+              </div>
             </div>
-            <button
-              type="submit"
-              className="ms-auto mt-5 block bg-cyan-700 px-10 py-2"
-            >
-              Send
-            </button>
           </form>
         </div>
       </section>
