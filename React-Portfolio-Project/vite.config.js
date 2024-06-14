da2a4b24e8a373a9599caecb267463a82548e0c4
Timothy Murphy <TimothyMurphy123@gmail.com>
2024-06-13 22:20:39 -0400

    added cssnano minification tool, autoprefixer for vendor fixes

--- React-Portfolio-Project/vite.config.js
+++ React-Portfolio-Project/vite.config.js
@@ -3,6 +3,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+import cssnano from 'cssnano';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,6 +16,8 @@ export default defineConfig({
     postcss: {
       plugins: [
         tailwindcss,
+        autoprefixer,
+        cssnano,
       ],
     },
   },
