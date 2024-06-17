507f13ec7071a53fc4706098ba6d901c7f951a9e
Timothy Murphy <TimothyMurphy123@gmail.com>
2024-06-17 00:33:33 -0400

    added fp-noscroll to projectspage

--- React-Portfolio-Project/src/components/pages/projects/ProjectsPage.jsx
+++ React-Portfolio-Project/src/components/pages/projects/ProjectsPage.jsx
@@ -10,7 +10,7 @@ import ProjPortfolio from './ProjPortfolio'
 export default function ProjectsPage() {
 
   return (
-    <div className="section fp-auto-height bg-gradient-to-b from-cyan-500 to-cyan-900">
+    <div className="section fp-auto-height fp-noscroll bg-gradient-to-b from-cyan-500 to-cyan-900">
       <div className="slide">
         <ProjGallery />
       </div>
