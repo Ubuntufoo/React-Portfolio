191338edac0656de60ad60a81a83c521f1eece2f
Timothy Murphy <TimothyMurphy123@gmail.com>
2024-07-08 10:33:50 -0400

    hover > image flip, layout improvements

--- React-Portfolio-Project/src/components/navbar/NavBarMobile.jsx
+++ React-Portfolio-Project/src/components/navbar/NavBarMobile.jsx
@@ -13,26 +13,26 @@ export default function NavBarMobile() {
   return (
     <nav
       id="NavBarMobile"
-      className={`fixed inset-x-0 top-0 z-50 flex flex-col bg-gray-700 text-center text-gray-50 transition-all duration-300 ${isOpen ? 'h-screen' : 'h-0'}`}
+      className={`fixed inset-x-0 top-0 z-50 flex flex-col bg-gray-700 text-center text-gray-50 transition-all duration-500 ${isOpen ? 'h-screen' : 'h-0'}`}
     >
       <div
         onClick={toggleNav}
-        className="absolute right-7 top-7 z-40 flex h-10 w-9 cursor-pointer flex-col items-center justify-center space-y-1.5 md:right-11 md:top-11 3xl:right-36 3xl:top-14"
+        className="group absolute right-7 top-7 z-40 flex h-10 w-9 cursor-pointer flex-col items-center justify-center space-y-1.5 md:right-11 md:top-11 3xl:right-36 3xl:top-14"
       >
         <div
-          className={`h-1.5 w-9 origin-right rounded bg-black transition-all duration-300 lg:h-2 lg:w-14 ${isOpen ? 'translate-y-[-5px] rotate-[-30deg] lg:translate-y-[-10px] lg:rotate-[-26deg]' : ''}`}
+          className={`h-1.5 w-9 z-1 origin-right rounded bg-black transition-all duration-500 lg:h-2 lg:w-12 ${isOpen ? 'translate-y-[-5px] rotate-[-30deg] lg:translate-y-[-13px] lg:rotate-[-36deg]' : ''}`}
         ></div>
         <div
-          className={`h-1.5 w-full origin-center rounded bg-gray-900 transition-all duration-300 lg:h-2 lg:w-14 ${isOpen ? 'translate-x-4 rotate-90 lg:translate-x-7' : ''}`}
+          className={`h-1.5 z-2 w-full origin-center rounded bg-gray-900 transition-all duration-500 lg:h-2 lg:w-14 ${isOpen ? 'translate-x-4 rotate-90 lg:translate-x-5' : ''} `}
         ></div>
         <div
-          className={`h-1.5 w-9 origin-right rounded bg-gray-800 transition-all duration-300 lg:h-2 lg:w-14 ${isOpen ? 'translate-y-[5px] rotate-[30deg] lg:translate-y-[11px] lg:rotate-[26deg]' : ''}`}
+          className={`h-1.5 z-3 w-9 origin-right rounded bg-gray-800 transition-all duration-500 lg:h-2 lg:w-12 ${isOpen ? 'translate-y-[5px] rotate-[30deg] lg:translate-y-[13px] lg:rotate-[36deg]' : ''}`}
         ></div>
       </div>
 
       <ul
         id="myMenu"
-        className={`transform ${isOpen ? 'translate-y-0' : '-translate-y-full'} duration-300 flex h-screen flex-col  items-center space-y-8 py-20 text-lg tracking-wide opacity-[.98] transition-transform sm:text-xl md:text-2xl`}
+        className={`transform ${isOpen ? 'translate-y-0' : '-translate-y-full'} flex h-screen flex-col items-center  space-y-8 py-20 text-lg tracking-wide opacity-[.98] transition-transform duration-300 sm:text-xl md:text-2xl`}
       >
         <div>
           <img
--- React-Portfolio-Project/src/components/pages/intro/Hero.jsx
+++ React-Portfolio-Project/src/components/pages/intro/Hero.jsx
@@ -1,17 +1,16 @@
 // hero section of intro page
 
-// import { useState } from 'react'
-import TimHero1 from '../../../assets/TimHero1.png'
-// import BG1 from '../../../assets/BG1.png'
+import { useState } from 'react'
+import Design1 from '../../../assets/Design1.png'
+import Design2 from '../../../assets/Design2.png'
 
 export default function Hero() {
+  const [imageSrc, setImageSrc] = useState(Design1)
+
   return (
     <main className="container mx-auto max-w-[1280px] bg-gray-400">
-      <section className="isolate flex h-screen flex-col place-content-evenly lg:place-content-center place-items-center pt-12 sm:gap-20 sm:text-lg md:gap-24 md:text-xl lg:flex-row lg:flex-wrap lg:gap-x-36 lg:ps-6 lg:pt-0 lg:text-2xl xl:gap-x-52 xl:ps-8 3xl:ps-12">
-        <div
-          className="relative flex w-1/2 flex-col gap-5 rounded-lg border border-gray-950 bg-gray-950 pt-5
-          tracking-wide sm:w-2/5 sm:gap-7  sm:pt-8 md:w-1/3 md:gap-9 md:pt-10 lg:ms-12 lg:w-3/10 lg:gap-10 lg:pt-12"
-        >
+      <section className="isolate flex h-screen flex-col place-content-evenly place-items-center pt-12 sm:gap-20 sm:text-lg md:gap-24 md:text-xl lg:flex-row lg:flex-wrap lg:place-content-center lg:gap-x-36 lg:ps-6 lg:pt-0 lg:text-2xl xl:gap-x-52 xl:ps-8 3xl:ps-12">
+        <div className="relative cursor-pointer flex w-1/2 flex-col gap-5 rounded-lg border border-gray-950 bg-gray-950 pt-5 tracking-wide sm:w-2/5 sm:gap-7 sm:pt-8 md:w-1/3 md:gap-9 md:pt-10 lg:ms-12 lg:w-1/4 lg:gap-10 lg:pt-12">
           <div className="absolute inset-0 -z-10 h-full w-full rotate-[33deg] rounded-xl border border-gray-950 bg-gray-900"></div>
           <div className="absolute inset-0 -z-20 h-full w-full rotate-[60deg] rounded-xl border border-gray-950 bg-gray-600"></div>
           <div className="absolute inset-0 -z-30 h-full w-full rotate-[90deg] rounded-xl border border-gray-950 bg-gray-300"></div>
@@ -19,14 +18,16 @@ export default function Hero() {
             Hi, I&apos;m Tim.
           </h1>
           <img
-            src={TimHero1}
+            src={imageSrc}
             alt="Tim's image as a cross hatch illustration"
-            className="mx-auto brightness-95"
+            className=" brightness-95 "
+            onMouseEnter={() => setImageSrc(Design2)}
+            onMouseLeave={() => setImageSrc(Design1)}
           />
         </div>
-        <div className="space-y-4 mt-4 text-center bg-gray-300 lg:bg-inherit w-full lg:basis-1/3 -z-10 py-5 lg:space-y-24">
+        <div className="-z-10 mt-4 w-full flex flex-col justify-center gap-4 bg-gray-300 h-1/3 text-center lg:basis-1/3 lg:space-y-24 lg:bg-inherit">
           <h2 className="whitespace-break-spaces">
-            Husband. Father.
+            Husband.   Father.
             <br />
             <span className="highlight mt-4 cursor-pointer font-semibold italic lg:mt-6">
               Web Developer.
@@ -44,10 +45,12 @@ export default function Hero() {
           <a
             href=""
             target="_blank"
-            className="group relative inline-flex items-center justify-center gap-2 overflow-hidden rounded-lg bg-black px-7 py-3 text-white focus:ring-4 focus:ring-blue-300"
+            className="group relative inline-flex items-center justify-center gap-2 overflow-hidden rounded-lg bg-black px-7 py-3 text-gray-50 duration-1000 hover:bg-blue-500"
           >
-            <span className="z-40">Explore</span>
-            <div className="absolute inset-0 z-20 h-[90%] w-[100%] translate-x-[-90%] rotate-45 bg-white/30 transition-all duration-1000 group-hover:translate-x-[90%] group-hover:scale-100"></div>
+            <span className="z-40 font-roboto font-semibold tracking-wider">
+              Explore
+            </span>
+            <div className="absolute inset-0 z-20 h-[90%] w-[100%] translate-x-[-95%] rotate-45 bg-gray-400 transition-all duration-1000 group-hover:translate-x-[90%] group-hover:scale-100"></div>
           </a>
         </div>
       </section>
