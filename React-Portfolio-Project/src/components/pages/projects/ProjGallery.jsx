f56104dabb8af55a40ff365e5285c49684569447
Timothy Murphy <TimothyMurphy123@gmail.com>
2024-07-09 12:26:16 -0400

    build for MVP projects page

--- React-Portfolio-Project/src/components/pages/projects/ProjGallery.jsx
+++ React-Portfolio-Project/src/components/pages/projects/ProjGallery.jsx
@@ -1,66 +1,59 @@
-// a list of projects in an accordion
+// a shifting grid of projects
 
-import { useState } from 'react'
+
 import ServerTooltip from '../../features/ServerTooltip'
-import { projMainContent } from '../../../utils/images'
 import { FaLink } from 'react-icons/fa'
 
+import { useState } from 'react'
+import { projMainContent } from '../../../utils/images'
+
 export default function ProjGallery() {
-  // const [featuredProj, setFeaturedProj] = useState(0)
+  const [featuredIndex, setFeaturedIndex] = useState(0)
+
+  const handleItemClick = (index) => {
+    if (index !== featuredIndex) {
+      setFeaturedIndex(index)
+    }
+  }
 
   return (
     <main className="relative flex h-screen flex-col justify-center">
-      <h1 className="absolute left-6 top-9 h-fit font-mono text-lg tracking-wide text-gray-950">
-        {'<Projects></Gallery>'}
+      <h1 className="absolute left-6 top-9 h-fit font-mono font-semibold text-xl tracking-wide text-gray-950 whitespace-break-spaces">
+        {`<Projects></Gallery>`}
       </h1>
 
       <section className="flex h-screen flex-col justify-center">
-        <div className="mx-auto mt-5 grid h-7/10 w-full grid-cols-2 place-content-evenly place-items-center gap-4 px-6 text-center">
-          <div className="col-span-full size-fit rounded-sm ring ring-gray-800">
-            <h2 className="hidden">{projMainContent[0].label}</h2>
+        <div className="h-7/10 mx-auto mt-5 grid w-full grid-cols-2 place-content-evenly place-items-center gap-4 px-6 text-center">
+          <div className="col-span-full size-fit rounded-sm ring ring-gray-800 transition-all duration-500 ease-in-out">
+            <h2 className="hidden">{projMainContent[featuredIndex].label}</h2>
             <img
-              src={projMainContent[0].src}
-              alt={projMainContent[0].label}
-              className=""
+              src={projMainContent[featuredIndex].src}
+              alt={projMainContent[featuredIndex].label}
+              className="transform transition-transform duration-500 ease-in-out hover:scale-105"
             />
           </div>
-          <div className="col-span-full mb-2.5 row-span-9 row-start-2 flex h-3/4 w-screen flex-col justify-center bg-gray-800">
+          <div className="col-span-full row-span-9 row-start-2 mb-2.5 flex h-3/4 w-screen flex-col justify-center bg-gray-800">
             <p className="mx-auto w-6/10 text-gray-50">
               Select an item, or scroll horizontally to view details
             </p>
           </div>
-          <div className="size-fit rounded-sm ring ring-gray-800">
-            <h2 className="hidden">{projMainContent[1].label}</h2>
-            <img
-              src={projMainContent[1].src}
-              alt={projMainContent[1].label}
-              className=""
-            />
-          </div>
-          <div className="size-fit rounded-sm ring ring-gray-800">
-            <h2 className="hidden">{projMainContent[2].label}</h2>
-            <img
-              src={projMainContent[2].src}
-              alt={projMainContent[2].label}
-              className=""
-            />
-          </div>
-          <div className="size-fit rounded-sm ring ring-gray-800">
-            <h2 className="hidden">{projMainContent[3].label}</h2>
-            <img
-              src={projMainContent[3].src}
-              alt={projMainContent[3].label}
-              className=""
-            />
-          </div>
-          <div className="size-fit rounded-sm ring ring-gray-800">
-            <h2 className="hidden">{projMainContent[4].label}</h2>
-            <img
-              src={projMainContent[4].src}
-              alt={projMainContent[4].label}
-              className=""
-            />
-          </div>
+          {projMainContent.map(
+            (item, index) =>
+              index !== featuredIndex && (
+                <div
+                  key={index}
+                  className="size-fit cursor-pointer rounded-sm ring ring-gray-800 transition-all duration-500 ease-in-out"
+                  onClick={() => handleItemClick(index)}
+                >
+                  <h2 className="hidden">{item.label}</h2>
+                  <img
+                    src={item.src}
+                    alt={item.label}
+                    className="transform transition-transform duration-500 ease-in-out hover:scale-105"
+                  />
+                </div>
+              ),
+          )}
         </div>
       </section>
     </main>
