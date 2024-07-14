c033b7387b1e9690bbe506339c3f418b53529b96
Timothy Murphy <TimothyMurphy123@gmail.com>
2024-07-13 20:46:40 -0400

    carousel spacing

--- React-Portfolio-Project/src/components/features/Carousel.jsx
+++ React-Portfolio-Project/src/components/features/Carousel.jsx
@@ -94,7 +94,7 @@ export default function Carousel({ images }) {
 
 
   return (
-    <div className="absolute bottom-[11%] overflow-hidden text-center">
+    <div className="absolute bottom-[14%] overflow-hidden text-center">
       <div
         ref={slidesContainerRef}
         className="slides mb-2.5 flex snap-x snap-mandatory overflow-x-auto scroll-smooth xl:py-2"
