ef63ce703f349d0d1b246bd060b476d22cfd6200
Timothy Murphy <TimothyMurphy123@gmail.com>
2024-07-25 16:43:37 -0400

    rework of nav layout

--- React-Portfolio-Project/src/components/features/KeyboardNoti.jsx
+++ React-Portfolio-Project/src/components/features/KeyboardNoti.jsx
@@ -1,22 +1,11 @@
 // an alert for keyboard users that goes invisible after 3 seconds
 
-import { useEffect, useState } from 'react'
 
 export default function KeyboardNoti() {
-  const [fade, setFade] = useState(true)
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setFade(false)
-    }, 10000)
-    return () => clearTimeout(timer)
-  }, [])
 
   return (
     <div
-      className={`${
-        fade ? 'opacity-100' : ' transition-display opacity-0 duration-1000'
-      } absolute bottom-2 right-7 hidden w-1/2 lg:w-1/3 2xl:w-1/4 cursor-default border-b-2 border-white pb-2 text-white ease-in`}
+      className={`italic col-span-2 w-full text-white`}
     >
       Keyboard Navigation Enabled
     </div>
--- React-Portfolio-Project/src/components/navbar/NavBarMobile.jsx
+++ React-Portfolio-Project/src/components/navbar/NavBarMobile.jsx
@@ -1,7 +1,12 @@
 // a hamburger toggle nav
 
 import { useState } from 'react'
-import TimLogoDraft2 from '../../assets/TimLogoDraft2.png'
+import KeyboardNoti from '../features/KeyboardNoti'
+import logo3 from '../../assets/logo3.png'
+import { FaGithub } from 'react-icons/fa'
+import { SiMinutemailer } from 'react-icons/si'
+import { BiLogoLinkedinSquare } from 'react-icons/bi'
+import { GrDocumentUser } from 'react-icons/gr'
 
 export default function NavBarMobile() {
   const [isOpen, setIsOpen] = useState(false)
@@ -13,7 +18,7 @@ export default function NavBarMobile() {
   return (
     <nav
       id="NavBarMobile"
-      className={`fixed inset-x-0 top-0 z-50 flex flex-col bg-gray-500 text-center text-gray-50 transition-all duration-500 ${isOpen ? 'h-screen' : 'h-0'}`}
+      className={`fixed inset-x-0 top-0 z-50 flex flex-col bg-gray-500 text-center text-gray-950 transition-all duration-500 ${isOpen ? 'h-screen' : 'h-0'}`}
     >
       <div
         onClick={toggleNav}
@@ -23,37 +28,68 @@ export default function NavBarMobile() {
           className={`h-1.5 w-9 z-1 origin-right rounded bg-black transition-all duration-500 lg:h-2 lg:w-12 ${isOpen ? 'translate-y-[-5px] rotate-[-29deg] lg:translate-y-[-13px] lg:rotate-[-36deg]' : ''}`}
         ></div>
         <div
-          className={`h-1.5 z-2 w-full origin-center rounded bg-gray-900 transition-all duration-500 lg:h-2 lg:w-14 ${isOpen ? 'translate-x-4 rotate-90 lg:translate-x-6' : ''} `}
+          className={`h-1.5 z-2 w-full origin-center rounded bg-gray-900 transition-all duration-500 lg:h-2 lg:w-14 ${isOpen ? 'translate-x-4 rotate-90 lg:translate-x-[21px]' : ''} `}
         ></div>
         <div
           className={`h-1.5 z-3 w-9 origin-right rounded bg-gray-800 transition-all duration-500 lg:h-2 lg:w-12 ${isOpen ? 'translate-y-[5px] rotate-[29deg] lg:translate-y-[13px] lg:rotate-[36deg]' : ''}`}
         ></div>
       </div>
 
-      <ul
-        id="myMenu"
-        className={`transform ${isOpen ? 'translate-y-0' : '-translate-y-full'} flex h-screen flex-col items-center  space-y-8 py-20 text-lg tracking-wide opacity-[.98] transition-transform duration-300 sm:text-xl md:text-2xl`}
-      >
-        <div>
+      <div className={`transform ${isOpen ? 'translate-y-0' : '-translate-y-full'} h-screen grid grid-cols-2 place-content-evenly place-items-center gap-y-6 transition-transform duration-300 relative text-xl`}>
+        <div className='w-fit space-y-2 col-span-2'>
           <img
-            src={TimLogoDraft2}
-            alt=""
-            className="absolute left-4 top-3 z-40 w-6/10 invert sm:w-2/5 md:left-6 md:top-5 lg:left-14 lg:top-6 lg:w-3/10"
+            src={logo3}
+            alt="logo of computer monitor on easel"
+            className="brightness-105 w-3/10 mx-auto"
           />
+          <p><code>&lt;Tim&gt; &lt;Murphy/&gt;</code></p>
         </div>
-        {['Intro', 'Projects', 'Skills', 'Contact'].map((section) => (
-          <li className="w-fit hover:scale-110 hover:font-bold" key={section}>
+
+
+        <ul
+          id
+          className={``}
+        >
+          <li className="">
             <a
-              data-menuanchor={`${section}`}
-              href={`#${section}`}
+              href="https://github.com/Ubuntufoo"
+              target="_blank"
+              rel="noopener noreferrer"
               className=""
-              onClick={toggleNav}
             >
-              {section.charAt(0).toUpperCase() + section.slice(1)}
             </a>
+            <FaGithub className="text-4xl" />
           </li>
-        ))}
-      </ul>
+          <li className="">
+            <a
+              href="mailto:TimothyMurphy123@gmail.com"
+              className=""
+            >
+            </a>
+            <SiMinutemailer className="text-4xl" />
+          </li>
+        </ul>
+
+
+        <ul
+          id="myMenu"
+          className={`space-y-10`}
+        >
+          {['Intro', 'Projects', 'Skills', 'Contact'].map((section) => (
+            <li className="w-full neumorph px-4 mx-auto py-2 rounded-xl font-kreon hover:scale-110 hover:font-bold transition" key={section}>
+              <a
+                data-menuanchor={`${section}`}
+                href={`#${section}`}
+                onClick={toggleNav}
+              >
+                {section.charAt(0).toUpperCase() + section.slice(1)}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <KeyboardNoti />
+      </div>
+
     </nav>
   )
 }
